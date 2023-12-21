647ee1390a03ed76b1c4ac11fa75259495038ad9
Alanas J <34601418+Alanas-J@users.noreply.github.com>
2023-12-21 01:13:34 +0000

    removed OOP spaghetti

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { Duration, Stack } from 'aws-cdk-lib';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 const app = new cdk.App();
-new CdkStack(app, 'PersonalSiteStack');
+const personalSiteStack = new Stack(app, 'PersonalSiteStack')
+
+// ******* Sample Services ***********
+const queue = new sqs.Queue(personalSiteStack, 'CdkQueue', {
+    visibilityTimeout: Duration.seconds(300)
+});
+const topic = new sns.Topic(personalSiteStack, 'CdkTopic');
+topic.addSubscription(new subs.SqsSubscription(queue));
